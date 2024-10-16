71934d91d305c94dfa34e2910f6e16193381d3e5
sara-mohamd <saraamohamed0038@gmail.com>
2024-10-16 16:48:55 +0300

    edit holbertonCourse class

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,16 +1,16 @@
 export default class HolbertonCourse {
   HolbertonCourse(name, length, students) {
-    if (typeof name === String)
+    if (typeof name === 'string')
       this._name = name;
     else
       throw TypeError('Name must be a string');
 
-    if (typeof length === Number)
+    if (typeof length === 'number')
       this._length = length;
     else
       throw TypeError('Length must be a number');
 
-    if (typeof students === Array)
+    if (Array.isArray(students))
       this._students = students;
     else
       throw TypeError('Students must be an array');
