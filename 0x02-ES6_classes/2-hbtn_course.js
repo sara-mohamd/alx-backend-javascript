0a6ef19a321910c84f9ddf9595b7b1325ae4b11a
sara-mohamd <saraamohamed0038@gmail.com>
2024-10-16 17:20:01 +0300

    adding accessor and mutator to each properte

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -15,4 +15,40 @@ export default class HolbertonCourse {
     else
       throw TypeError('Students must be an array');
   }
+
+  // name's accessor and Mutator
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string')
+      this._name = newName;
+    else
+      throw TypeError('Name must be a string');
+  }
+
+  // length's accessor and Mutator
+  get length() {
+    return this_length;
+  }
+
+  set length(newLength) {
+    if (typeof length === 'number')
+      this._length = length;
+    else
+      throw TypeError('Length must be a number');
+  }
+
+  // students'accessor and Mutator
+  get students() {
+    return this_students;
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(students))
+      this._students = students;
+    else
+      throw TypeError('Students must be an array');
+  }
 }
